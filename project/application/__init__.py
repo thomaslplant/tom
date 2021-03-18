cb891ae321e9684fc7086d6b1f26654738b5e777
Tom Plant <thomaslplant@gmail.com>
2021-03-18 21:36:28 +0000

    Rearranged the database so it is easily readable, also started the testing and is now at 76%

---
+++ project/application/__init__.py
@@ -0,0 +1,16 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__, template_folder='../templates')
+
+
+# app.config['SQLALchemy_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@34.89.77.106/toms_project_bar"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+
+db = SQLAlchemy(app)
+
+from application import routes
